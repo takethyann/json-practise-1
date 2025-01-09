5e0da17979253b8ceb07423fcc97861a5bb36830
takethyann <i.take.mg.c@outlook.jp>
2025-01-09 17:53:31 +0900

    課題　for文を利用したjsonデータ操作２

---
+++ work/js/app.js
@@ -0,0 +1,2 @@
+document.addEventListener(`DOMContentLoaded`,() => fetch(`data/product.json`).then(Response => Response.json()).then(data => data.forEach(product => document.getElementById(`productTable`).appendChild(Object.assign(document.createElement(`tr`),
+{innerHTML:`<td>${product.id}</td><td>${product.name}</td><td>${product.price}</td><td>${product.description}</td>`})))) .catch(error => console.error('エラー:', error)));
