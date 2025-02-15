f199957575de7cdb909ca07f31b3a52bcb9c1104
takethyann <i.take.mg.c@outlook.jp>
2025-02-15 23:42:45 +0900

    JSONデータ操作2 課題

--- work/js/app.js
+++ work/js/app.js
@@ -1,2 +1,19 @@
-document.addEventListener(`DOMContentLoaded`,() => fetch(`data/product.json`).then(Response => Response.json()).then(data => data.forEach(product => document.getElementById(`productTable`).appendChild(Object.assign(document.createElement(`tr`),
-{innerHTML:`<td>${product.id}</td><td>${product.name}</td><td>${product.price}</td><td>${product.description}</td>`})))) .catch(error => console.error('エラー:', error)));
+document.addEventListener("DOMContentLoaded",function() {
+    fetch("data/product.json")
+    .then(response => response.json())
+    .then(products => {
+        const tableBody = document.getElementById("productTable");
+        
+        products.forEach(product => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>${product.description}</td>`;
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('エラー:', error));
+});
